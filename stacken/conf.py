ceacad44d1eea32ce5b602f226d803c9dfe25b5e
Stefan Berggren <nsg@nsg.cc>
2018-12-08 16:06:40 +0100

    Enable Galleries

--- stacken/conf.py
+++ stacken/conf.py
@@ -672,7 +672,7 @@ GITHUB_COMMIT_SOURCE = False
 # "source/" and the results will be located in
 # "OUTPUT_PATH/relative_destination/gallery_name"
 # Default is:
-# GALLERY_FOLDERS = {"galleries": "galleries"}
+GALLERY_FOLDERS = {"galleries": "galleries"}
 # More gallery options:
 # THUMBNAIL_SIZE = 180
 # MAX_IMAGE_SIZE = 1280
@@ -680,7 +680,7 @@ GITHUB_COMMIT_SOURCE = False
 # EXTRA_IMAGE_EXTENSIONS = []
 #
 # If set to False, it will sort by filename instead. Defaults to True
-# GALLERY_SORT_BY_DATE = True
+GALLERY_SORT_BY_DATE = True
 
 # If set to True, EXIF data will be copied when an image is thumbnailed or
 # resized. (See also EXIF_WHITELIST)
